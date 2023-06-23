d03c56da22f8f06eb743ade4dfc2c83ea2177525
Ivan Ivanov <101635841+vin-57@users.noreply.github.com>
2023-06-23 09:23:14 +0300

    Create model_testing.py

---
+++ lab3/model_testing.py
@@ -0,0 +1,19 @@
+# Import required libraries
+import pandas as pd
+import pickle
+
+
+# Loading preprocessed test data from a file
+test_data = pd.read_csv('test_preprocessed/test_data_preprocessed.csv')
+
+# Loading a model from the model.pkl file
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# Model evaluation on test data
+X_test = test_data[['price', 'delivery_time', 'number_of_transit_nodes']]
+y_test = test_data['label']
+accuracy = model.score(X_test, y_test)
+
+# Output of test results
+print("Model test accuracy is:", accuracy)
