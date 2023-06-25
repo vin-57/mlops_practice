f6196e2a7e5935af70347c069510b315cf50bdbf
Anton <80177461+smirnovanton90@users.noreply.github.com>
2023-06-25 10:14:10 +0300

    Create data_preprocessing1.py

---
+++ lab4/data_preprocessing1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+# Загрузка датасета
+df = pd.read_csv('datasets/data.csv')
+
+# Выбор необходимых столбцов
+df = df[['price', 'delivery_time', 'number_of_transit_nodes','departure_city']]
+
+# Сохранение модифицированного датасета
+df.to_csv('datasets_storage/data_prep1.csv', index=False)
